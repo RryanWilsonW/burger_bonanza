c7e5af8096cbb916b22aa55131f2647a4a7803c3
Ryan Wilson <RryanWilsonW@gmail.com>
2021-04-02 03:50:12 -0400

    routes set up to query the database

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,57 @@
+// Requires our dependencies.
 const express = require('express');
 const burger = require('../models/burger.js');
 
-const app = express();
+// Creates and exports the router for the file.
+const router = express.Router();
+
+// Will grab index.html file, and render it to the user.
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+// Inserts new column into out burgers table.
+router.post('/api/burgers', (req, res) => {
+    burger.insertOne(['burger_name', 'devoured'], 
+    [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+// Updates one based off of its corresponding id.
+router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id ${req.params.id}`;
+    console.log('condition', condition);
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if(result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        };
+    });
+});
+
+// Delete one based on its corresponding id.
+router.delete('/api/burgers/:id', (req, res) => {
+    const condition = `id ${req.params.id}`;
+    console.log('condition', condition);
+
+    burger.deleteOne(condition, (result) => {
+        if(result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
+
 
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-const orm = requiree('orm');
+const orm = require('../config/orm');
 
 let data = orm.selectAll('burger_name', 'burgers', (result) =>
     console.log(result)
