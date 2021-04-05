d454d3463b1206a7528b6d96ddbc49f7fdf7f6c7
Ryan Wilson <RryanWilsonW@gmail.com>
2021-04-05 15:55:18 -0400

    stylesheet working after static route fixed at server.js 8

--- models/burger.js
+++ models/burger.js
@@ -5,6 +5,18 @@ const burger = {
         orm.selectAll('burgers', (res) => {
             cb(res);
         });
+    },
+
+    insertOne: (cols, vals, cb) => {
+        orm.insertOne('burgers', cols, vals, () => {
+            cb(res)
+        });
+    },
+
+    updateOne: (objColVals, condition, cb) => {
+        orm.updateOne('burgers', objColVals, condition, (res) => {
+            cb(res)
+        });
     }
 };
 
