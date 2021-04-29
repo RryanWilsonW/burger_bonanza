f2e9ace1bada8e20a86d34ac8184c43483b1be16
Ryan Wilson <RryanWilsonW@gmail.com>
2021-04-29 14:47:49 -0400

    After redoing the index.handlebars page, I finally was able to make the devour bttn work!

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,34 @@
-$('.eatBurger').on('click', (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+      console.info('DOM loaded');
+    }
+  
+    const devourBttns = document.querySelectorAll('.devour-bttn');
+     if (devourBttns) {
+         devourBttns.forEach((button) => {
+             button.addEventListener('click', (event) => {
+                 const id = event.target.getAttribute('data-id');
+                 const newDevour = event.target.getAttribute('data-newdevour');
 
-    let id = $(this).data('id')
-    console.log(id)
-    //let devouredState = {
-        // devoured = 1
-    //}
+                 const newDevourState = {
+                     devoured: newDevour
+                 };
+                 fetch(`/burgers/${id}`, {
+                     method: 'PUT',
+                     headers: {
+                         Accept: 'application/json',
+                         'Content-Type': 'application/json',
+                     },
+                     body: JSON.stringify(newDevourState),
+                 }).then((response) => {
+                     if( response.ok) {
+                         console.log(`Burger Devoured`);
+                         location.reload('/');
+                     } else {
+                         alert('You messed up kid!')
+                     }
+                 })
+             })
+         })
+     }
 })
