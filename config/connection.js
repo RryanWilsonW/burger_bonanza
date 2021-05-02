ab7cc6bc9440f321c98c7172ed5fd40584dc021d
Ryan Wilson <RryanWilsonW@gmail.com>
2021-05-02 15:28:15 -0400

    update db connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'C@rb0n2020',
-    database: 'burgers_db',
-  });
+
+//const connection = mysql.createConnection({
+//    host: 'localhost',
+//    port: 3306,
+//    user: 'root',
+//    password: 'C@rb0n2020',
+//    database: 'burgers_db',
+//  });
   
+  const connection = mysql.createConnection(process.env.DATABASE_URL);
+
   // Makes a connection.
   connection.connect((err) => {
     if (err) {
